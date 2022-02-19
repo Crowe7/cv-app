438f2b830d0af22a26ca51e99fe6b03c2d673dfa
Crowe7 <jacobocrowe@gmail.com>
2022-02-18 23:05:16 -0600

    Added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './styles/App.css'
+import Header from './components/Header';
 
 class App extends Component {
   constructor(props:any) {
@@ -12,7 +13,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>hello</h1>
+        <Header/>
       </div>
     )
   }
