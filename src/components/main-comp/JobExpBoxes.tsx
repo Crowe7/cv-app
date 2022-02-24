eac7d6f95b46a06d886def4f7853700d7ddff74e
Crowe7 <jacobocrowe@gmail.com>
2022-02-23 23:56:36 -0600

    added delete button onto the job exp div

--- src/components/main-comp/JobExpBoxes.tsx
+++ src/components/main-comp/JobExpBoxes.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { JobInfo } from './JobExp'
 import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 type MyProps = {
   InfoCount: JobInfo[],
@@ -10,13 +12,18 @@ type MyProps = {
 
 //this.props.handleChange?.(info.ID) is called optional chaining
 export default class JobExpBoxes extends Component<MyProps> {
-
+//https://stackoverflow.com/questions/37387351/reactjs-warning-setstate-cannot-update-during-an-existing-state-transiti
   render() {
-    const {InfoCount, handleChange} = this.props
+    const {InfoCount, handleChange, handleDelete} = this.props
     return (
       <div>
           {InfoCount.map((info) => {
-              return <TextField key={info.ID} onChange={handleChange?.(info.ID)} name="Title" id="outlined-basic" label="Title" variant="outlined" />;
+              return <div key={info.ID}>
+                          <IconButton aria-label="delete" onClick={() => handleDelete?.(info.ID)}>
+                            <DeleteIcon />
+                          </IconButton>
+                          <TextField onChange={handleChange?.(info.ID)} name="Title" id="outlined-basic" label="Title" variant="outlined" />
+                    </div>;
             })
           }
       </div>
--- src/components/main-comp/jobExp.tsx
+++ src/components/main-comp/jobExp.tsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import { v4 as uuidv4 } from 'uuid';
 import Button from '@mui/material/Button';
 import JobExpBoxes from "./JobExpBoxes";
+
 export interface JobInfo {
   Title: string
   Employer: string
