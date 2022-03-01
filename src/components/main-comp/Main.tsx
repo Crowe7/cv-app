f3886c79d1e59bc06950876c6594bb0407671cd0
Crowe7 <jacobocrowe@gmail.com>
2022-03-01 16:04:57 -0600

    lifted header state to app

--- src/components/main-comp/Main.tsx
+++ src/components/main-comp/Main.tsx
@@ -2,16 +2,26 @@ import React, { Component } from 'react'
 import JobExp from './JobExp'
 import GeneralInfo from './GeneralInfo'
 // might have to create a state here that renders the main exp if on true and cv if on false, and pass that to the header so the switch is a child oh main still
-export default class Main extends Component {
 
-
-
+type MyProps = {
+  Mode: boolean | undefined
+}
+export default class Main extends Component<MyProps> {
   render() {
+    const {Mode} = this.props
+    function changeMode() {
+      if(Mode) {
+        return <div>
+                <GeneralInfo/>
+                <JobExp/>
+               </div>
+      } else {
+        return <div>PlaceHolder</div>
+      }
+    }
+
     return (
-      <div>
-        <GeneralInfo/>
-        <JobExp/>
-      </div>
+      changeMode()
     )
   }
 }
