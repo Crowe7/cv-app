b3852133af0ad17289180789f0451dc662904bbb
Crowe7 <jacobocrowe@gmail.com>
2022-03-06 23:53:07 -0600

    added basic styles for preview sheet

--- src/components/main-comp/CvPreview.tsx
+++ src/components/main-comp/CvPreview.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { GeneralInfoState } from './Main'
 import { JobInfo } from './Main'
 import { EducationInfo } from './Main'
+import Box from '@mui/material/Box'
 
 type MyProps = {
     JobInfo: JobInfo[],
@@ -13,7 +14,23 @@ export default class CvPreview extends Component<MyProps> {
   render() {
     const {JobInfo, EducationInfo, GeneralInfo} = this.props
     return (
-      <div>Map out each bit of info then style</div>
+      <Box sx={{border: 1, height: "1000px", width: "210mm", margin: "20px" }} className='pageSize'>
+        <Box sx={{padding: "10px", borderBottom: 3, borderColor: 'black', width: "80%"}}>
+          <h1>General</h1>
+        </Box>
+        <Box sx={{padding: "10px", borderBottom: 3, borderColor: 'black'}}>
+        </Box>
+        <Box sx={{padding: "10px", borderBottom: 3, borderColor: 'black', width: "80%"}}>
+          <h1>Education</h1>
+        </Box>
+        <Box sx={{padding: "10px", borderBottom: 3, borderColor: 'black'}}>
+        </Box>
+        <Box sx={{padding: "10px", borderBottom: 3, borderColor: 'black', width: "80%"}}>
+          <h1>Job Experience</h1>
+        </Box>
+        <Box sx={{padding: "10px"}}>
+        </Box>
+      </Box>
     )
   }
 }
